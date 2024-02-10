91598952b28f6c143de42f55058d743beb9d14be
Mudae <mudae@Ubuntu.myguest.virtualbox.org>
2024-02-10 02:49:15 -0800

    working file

--- base.cpp
+++ base.cpp
@@ -1,23 +1,28 @@
 #include <sys/types.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <sys/wait.h>
 
-int main() {
+int main()
+{
   pid_t pid;
   /* fork a child process */
   pid = fork();
-  
-  if (pid < 0) { /* error */
+
+  if (pid < 0)
+  { /* error */
     fprintf(stderr, "Fork Failed");
     return 1;
   }
-  else if (pid == 0) { /* child process */
+  else if (pid == 0)
+  { /* child process */
     execlp("/bin/ls", "ls", NULL);
   }
-  else { /* parent process */
+  else
+  { /* parent process */
     /* parent will wait for the child to complete */
-    wait(null);          // If this line is ommitted, child process is a zombie
-    printf("child complete");
+    wait(NULL); // If this line is ommitted, child process is a zombie
+    printf("child complete\n");
   }
   return 0;
 }
